ad7bc4649cd1132f4d16092eb0318f7bd6705c45
Anna Kozak <anna.kozak61@gmail.com>
2023-01-26 17:14:20 +0100

    Zmiany w wyglądzie aplikacji. Dodanie nowej funkcjonalności.

--- src/main/java/com/example/drogeria/Controller.java
+++ src/main/java/com/example/drogeria/Controller.java
@@ -8,21 +8,27 @@ public class Controller {
 
 
     @GetMapping("/")
-    public String app(){
+    public String app() {
         return "index.html";
     }
 
     @GetMapping("/naszeLakiery")
-    public String lakiery(){
+    public String lakiery() {
         return "lakiery.html";
     }
 
     @GetMapping("/naszeZele")
-    public String zele(){
+    public String zele() {
         return "zele.html";
     }
+
     @GetMapping("/naszeAkcesoria")
-    public String akcesoria(){
+    public String akcesoria() {
         return "akcesoria.html";
+    }
+
+    @GetMapping("/back")
+    public String back(){
+        return "index.html";
     }
 }
